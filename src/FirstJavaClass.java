280aff60c2058151f99c7f28bfa43d35224b93fa
Shebnem Bayezit <sebnembayezit2022@gmail.com>
2022-03-26 15:58:31 -0400

    my first day in github

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -2,5 +2,6 @@ public class FirstJavaClass {
 
     public static void main(String[] args) {
         System.out.println("learning github");
+        System.out.println("My first day in github");
     }
 }
