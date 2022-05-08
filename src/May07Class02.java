73f33b56c748dc8179bdc13623829a2c8143e7a3
Shebnem Bayezit <sebnembayezit2022@gmail.com>
2022-05-07 22:19:15 -0400

    day03_May07class02

---
+++ src/May07Class02.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class May07Class02 {
+
+    public static void main(String[] args) {
+        String str="123456789";
+        System.out.println("\"123456789\" ");
+    }
+
+
+}
